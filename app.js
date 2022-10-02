35f67708f90b43e7da26e71d956f99dd68faa36a
rodrigopetroni <RM93872@fiap.com.br>
2022-10-01 22:09:50 -0300

    Criação do app e do index.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PetForm from "./components/FormPet";
+
+const App = () => {
+  return (
+    <>
+      <h1>Dados do Pet</h1>
+      <PetForm />
+    </>
+  );
+};
+
+export default App;
